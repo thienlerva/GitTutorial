a9f2358b7fd0165c8f1cc9877dbed92993c05b02
Thien <thienxuanle@yahoo.com>
2019-10-28 20:09:00 -0400

    fourth

--- src/Git.java
+++ src/Git.java
@@ -7,6 +7,7 @@ public class Git {
 		System.out.println("First line of code");
 		System.out.println("Second line of code");
 		System.out.println("Third line of code");
+		System.out.println("Fourth line of code");
 	}
 
 }
