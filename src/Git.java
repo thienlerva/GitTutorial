333729bcdcd151d897ced425f3f542325283665a
Thien <thienxuanle@yahoo.com>
2019-10-28 20:29:10 -0400

    err

--- src/Git.java
+++ src/Git.java
@@ -6,14 +6,11 @@ public class Git {
 		
 		System.out.println("First line of code");
 		System.out.println("Second line of code");
-<<<<<<< HEAD
 		System.out.println("First line of code in feature branch");
 		System.out.println("Second line of code in feature branch");
-=======
 		System.out.println("Third line of code");
 		System.out.println("Fourth line of code");
 		System.out.println("Fifth line of code");
->>>>>>> master
 	}
 
 }
