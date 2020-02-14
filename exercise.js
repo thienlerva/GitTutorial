8427e6e0aa6966c7f99d39d741e5568387a62210
Thien <thienxuanle@yahoo.com>
2020-02-14 10:32:07 -0500

    added method in exercise.js

--- exercise.js
+++ exercise.js
@@ -1,1 +1,16 @@
 $('#fiboSubmit').on('click', runFib);
+
+function runFib() {
+    var n = $('#fiboInput').val();
+    $('#fiboOutput').html(fibonacci(n));
+    
+    var tom1 = new Audio("sounds/tom-1.mp3");
+      tom1.play();
+}
+
+function fibonacci(n) {
+    if (n==0 || n==1) {
+        return n;
+    }
+    return fibonacci(n-1) + fibonacci(n-2);
+}
